d47703d362d19cd8501f30c1f88a671820ff72dd
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2022-11-05 13:32:55 +0530

    gjh

--- src/configuration.h
+++ src/configuration.h
@@ -59,7 +59,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RANDOMX_PROGRAM_ITERATIONS 16384
 
 //Number of chained VM executions per hash.
-#define RANDOMX_PROGRAM_COUNT      1
+#define RANDOMX_PROGRAM_COUNT      2
 
 //Scratchpad L3 size in bytes. Must be a power of 2.
 #define RANDOMX_SCRATCHPAD_L3      2097152
